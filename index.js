d57839d82065e3641ac9228789350afb3eb4923f
Vitali Karpuk <karpuk-job@yandex.ru>
2019-10-06 12:08:20 +0300

    заглушка yoga

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const inpunName = document.getElementsByClassName('input_name')[0];
-const inputPhonNumber = document.getElementsByClassName('input_phonNumber')[0];
-const btnRequest = document.getElementsByClassName('btn_request')[0];
-const name = '';
-const phonNumber = '';
-console.log(inputPhonNumber.name)
-inpunName.onchange = (e) => name.push(e.target.value)
-inputPhonNumber.onchange = (e) => phonNumber.push(e.target.value)
-console.log(name)
-btnRequest.onclick = (e) => {
-    inpunName.name = name
-    inputPhonNumber.name = phonNumber
-}
+// const inpunName = document.getElementsByClassName('input_name')[0];
+// const inputPhonNumber = document.getElementsByClassName('input_phonNumber')[0];
+// const btnRequest = document.getElementsByClassName('btn_request')[0];
+// const name = '';
+// const phonNumber = '';
+// console.log(inputPhonNumber.name)
+// inpunName.onchange = (e) => name.push(e.target.value)
+// inputPhonNumber.onchange = (e) => phonNumber.push(e.target.value)
+// console.log(name)
+// btnRequest.onclick = (e) => {
+//     inpunName.name = name
+//     inputPhonNumber.name = phonNumber
+// }
