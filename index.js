7ae9533d7267babe2be80354565f38d82cd256de
Vitali Karpuk <karpuk-job@yandex.ru>
2019-10-07 05:05:54 +0300

    заглушка yoga

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// const inpunName = document.getElementsByClassName('input_name')[0];
-// const inputPhonNumber = document.getElementsByClassName('input_phonNumber')[0];
-// const btnRequest = document.getElementsByClassName('btn_request')[0];
-// const name = '';
-// const phonNumber = '';
-// console.log(inputPhonNumber.name)
-// inpunName.onchange = (e) => name.push(e.target.value)
-// inputPhonNumber.onchange = (e) => phonNumber.push(e.target.value)
-// console.log(name)
-// btnRequest.onclick = (e) => {
-//     inpunName.name = name
-//     inputPhonNumber.name = phonNumber
-// }
+window.addEventListener(orientationEvent, function() {
+    var orientation = Math.abs(window.orientation) == 90 ? 'landscape' : 'portrait';
+}, false);
